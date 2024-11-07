da2ee6da41845670e6ec5d22ff7b0edf1a3060dc
Miouzora <alexandre.franquet@epitech.eu>
2024-11-08 00:00:07 +0900

    a

--- xmake.lua
+++ xmake.lua
@@ -7,7 +7,7 @@ add_includedirs("src")
 target("my_package")
     set_kind("static")
 
-    add_files("src/foo.c")
+    add_files("src/foo.cpp")
     add_headerfiles("src/foo.h")
 
     add_includedirs("src", {public = true})
