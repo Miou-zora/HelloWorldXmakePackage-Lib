3d7a89e8a4da77cc1ed8aade8d52f62c4b7e43ce
Miouzora <alexandre.franquet@epitech.eu>
2024-11-08 16:29:57 +0900

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,12 +2,10 @@ set_project("my_package")
 set_languages("c++20")
 set_version("1.0.0")
 
-add_includedirs("src")
+add_includedirs("src", {public = true})
 
 target("my_package")
     set_kind("static")
 
     add_files("src/foo.cpp")
     add_headerfiles("src/foo.h")
-
-    add_includedirs("src", {public = true})
